5c2cd47416ca0cbb3116c71cc9758aaa435e2a66
Ishafizan Ishak <ishafizan@gmail.com>
2018-08-21 16:09:44 +0800

    remove token details

--- learning/crypto_crawl/crawl/settings.py
+++ learning/crypto_crawl/crawl/settings.py
@@ -2,21 +2,8 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-import sys
 
 __author__ = 'Ishafizan'
 
-# import sys
-sys.path.append('/usr/local/lib/python3.7/site-packages')
-# sys.path.append('/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload')
-
 # TOKENS
-TOKEN_QUANDL = "LmAihpoTW7EyzaRzxgZ8"
-
-ERROR_CNT = 5
-PAGING_LIMIT = 5
-ITEM_FEED_LIMIT = 50
-
-RECONNECT_CNT = 2
-REQUEST_TIMEOUT = 10
-REC_SIZE = 20
+TOKEN_QUANDL = "##########"
