7da12df7de2ec4fff814edd5a778b13fb77349b7
Ishafizan Ishak <ishafizan@gmail.com>
2018-08-27 18:56:07 +0800

    Update settings.py

--- learning/hello_world/deploy/settings.py
+++ learning/hello_world/deploy/settings.py
@@ -10,14 +10,14 @@ __contract_path__ = "../contracts/"
 __contract_filename__ = 'contract.json'
 
 # for deployment & retrieval
-__infura_rinkeby__ = "https://rinkeby.infura.io/v3/9e44e05985cf443a99ebad74d4ae194d"
-__contract_address__ = "0x930ecB8161a2b0ED0C019B8459C3b7ff24758b6e"
-# https://rinkeby.etherscan.io/address/0x930ecB8161a2b0ED0C019B8459C3b7ff24758b6e
+__infura_rinkeby__ = "https://rinkeby.infura.io/v3/<your_key>"
+__contract_address__ = "<deployed contract addr>"
+# https://rinkeby.etherscan.io/address/<deployed contract addr>
 # need to assign proper chain ID for different test nets https://github.com/ethereum/web3.js/issues/1040
 __chain_id__ = 4
 
 # metamask
-__wallet_private_key__ = "7758E6DF4954AC443C9B91F0860F850EE551728B457F1B2A2F8944BCCA373F7A"
-__wallet_address__ = "0x65ba359b2f70a107339c45f000105bb86b27f11b"
+__wallet_private_key__ = "wallet private key"
+__wallet_address__ = "<wallet account addr>"
 
 
