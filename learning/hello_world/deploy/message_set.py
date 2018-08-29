18a021b618626ff4d681964bafb64c1cf666b3a7
Ishafizan Ishak <ishafizan@gmail.com>
2018-08-29 18:18:39 +0800

    Update message_set.py

--- learning/hello_world/deploy/message_set.py
+++ learning/hello_world/deploy/message_set.py
@@ -45,24 +45,26 @@ except Exception as err:
 
 # call function & set message
 try:
+    gas_estimate = greeter.functions.setGreeting(set_message).estimateGas()
+    log.info("Gas estimate to transact with setVar: {0}".format(gas_estimate))
     log.info("call function & set message: %s" % set_message)
-    wallet_address = util_web3.addr_checksum(log, settings.__wallet_address__, w3)
-    nonce = w3.eth.getTransactionCount(wallet_address)
-    txn_dict = greeter.functions.setGreeting(set_message).buildTransaction({
-        'chainId': chain_id,
-        'gas': 140000,
-        'gasPrice': w3.toWei('21', 'gwei'),
-        'nonce': nonce,
-    })
-    # Submit the transaction that sets message
-    signed_txn = w3.eth.account.signTransaction(txn_dict, settings.__wallet_private_key__)
-    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-    # Wait for transaction to be mined...
-    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    # log.info(tx_receipt)
+    if gas_estimate > 100000:
+        log.warn("gas cost exceeds 100K")
+    else:
+        wallet_address = util_web3.addr_checksum(log, settings.__wallet_address__, w3)
+        nonce = w3.eth.getTransactionCount(wallet_address)
+        txn_dict = greeter.functions.setGreeting(set_message).buildTransaction({
+            'chainId': chain_id,
+            'nonce': nonce,
+        })
+        # Submit the transaction that sets message
+        signed_txn = w3.eth.account.signTransaction(txn_dict, settings.__wallet_private_key__)
+        tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+        # Wait for transaction to be mined...
+        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        # log.info(tx_receipt)
 except Exception as err:
     log.error(err)
-
 
 # -- END
 end = time()
